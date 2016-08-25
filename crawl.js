bf69321542ebbef7bb32e1ab0fedc37ecb9fa2e8
Eric Willis <admin@ecwillis.com>
2016-08-25 07:25:03 +0000

    Really fixed call stack no return error and added option `--no-host`

--- crawl.js
+++ crawl.js
@@ -1,72 +1,92 @@
 'use strict';
 
-const rootUrl = process.argv.pop();
+const argv = require('yargs').argv;
+
+const rootUrl = argv._.pop();
 const request = require('request');
 const async = require('async');
 const _ = require('lodash');
 const $ch = require('cheerio');
 
 
+let reqCount = 0;
 const crawlCache = [];
 
 const crawlPage = function(url, cb) {
+  const fullUrl = `${rootUrl}${url}`;
+
   crawlCache.push(url);
-  console.log(`${url}`);
+
+  if (argv.host === false) {
+    console.log(`${url}`);
+  } else {
+    console.log(`${fullUrl}`);
+  }
+
+  reqCount++;
+  if (reqCount > 5000) {
+    // Failsafe to prevent runnaway scripts
+    return cb(`REQUEST COUNT ${reqCount} EXCEDED!`);
+  }
+
   request({
-    headers:  {
+    headers: {
       'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.75 Safari/537.36'
     },
-    url
+    url: fullUrl
   }, (err, resp, body) => {
-   
-    if ( err ) {
+    if (err) {
       cb(err);
       return;
     }
-    
+
     if (resp.statusCode !== 200) {
       // console.log(`\tStatus Code${resp.statusCode}`);
       return cb();
     }
 
-    if(resp.headers['content-type'].indexOf('text/html') === -1) {
+    if (resp.headers['content-type'].indexOf('text/html') === -1) {
       // console.log('Non HTML Document');
       return cb();
     }
-    
+
     const $ = $ch.load(body);
 
-    async.eachLimit($('a'), 5, (elem, next) => {
+    const anchors = $('a');
+
+    async.eachLimit(anchors, 5, (elem, next) => {
       const ref = elem.attribs.href;
-      
-      if(!ref) {
+
+      if (!ref) {
         return next();
       }
 
-      if(ref[0] === '/' || ref.indexOf(rootUrl) !== -1 || ref !== '/') {
+      if ((ref[0] === '/' || ref.indexOf(rootUrl) !== -1) && ref !== '/') {
         let rootRef = ref.replace(`${rootUrl}/`, '');
-        if (rootRef !== '' && rootRef[0] !== '#') {
+        rootRef = rootRef.replace(/#[A-Za-z\-]+/g, '');
+
+        if (rootRef !== '') {
           if (rootRef[0] === '/') {
             rootRef = rootRef.substr(1);
           }
 
-          rootRef = rootRef.replace(/\#[A-Za-z\-]+/g, '');
-          const nextPage = `${rootUrl}/${rootRef}` // An actual page on this domain
+          const nextPage = `/${rootRef}`; // An actual page on this domain
           if (_.indexOf(crawlCache, nextPage) !== -1) {
             return next();
           }
-          
-          crawlPage(nextPage, next);
+
+          return crawlPage(nextPage, next);
         }
       }
-    }, cb);
 
-  }); 
+      return next();
+    }, cb);
+  });
 };
 
 
-crawlPage(rootUrl, (err) => {
-  if(err) {
+crawlPage('', (err) => {
+  if (err) {
     console.log(err);
   }
 
